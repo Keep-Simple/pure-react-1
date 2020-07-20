5c29558fdd2c232a23ec962397b15c3dc387ff11
nick <nick>
2020-07-20 16:58:17 +0300

    init

---
+++ src/Chat.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Chat = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <p>Chat For Bsa</p>
+      </header>
+    </div>
+  );
+}
+
+export default Chat;
